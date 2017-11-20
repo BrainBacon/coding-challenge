8d305754c214a6eaf17fa0ed05ae761e19e813aa
Brian Jesse <brian.jesse@gmail.com>
2017-11-20 13:43:57 -0600

    build(webpack): newest version of begin-build

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 let build = require('begin-build');
 
-// HACK: vue-loader exports an ES module and doesn't support CommonJS
 module.exports = build({
-  // components: { app: require('./app/vue.pug').default },
-  components: { app: require('./app/vue.pug').default },
+  components: { app: require('./app/vue.pug') },
 });
 
